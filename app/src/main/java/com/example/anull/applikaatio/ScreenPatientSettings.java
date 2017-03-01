cda97cdc5a9aea81027bafe825fa404c17530d17
Antti Leppälä <antti.leppaelae@gmail.com>
2017-03-01 12:50:19 +0200

    Clean uppia ja säätämistä. Shared Preferences ei pelaa, syy tuntematon

--- app/src/main/java/com/example/anull/applikaatio/ScreenPatientSettings.java
+++ app/src/main/java/com/example/anull/applikaatio/ScreenPatientSettings.java
@@ -22,7 +22,6 @@ public class ScreenPatientSettings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.patientscreen);
-
     }
 
     /*
